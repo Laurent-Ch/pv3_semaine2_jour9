af566fc9ea82a676b9e2948d7c8d42cd9f257419
Laurent-Ch <chamouleau.laurent@gmail.com>
2021-10-07 18:23:02 +0200

    adding the exercices of the day

--- 00_journalists_push.rb
+++ 00_journalists_push.rb
@@ -2,7 +2,7 @@
 raw_data = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
 
 
-# 1. Number of elements (possible methods: size, length, count)
+# 1. Number of elements
 def number_of_elements(data)
 	number = data.length
 	puts "The number of handles is #{number}"
@@ -12,7 +12,7 @@ end
 # 2. Shortest element in the list
 # Technique gourmande en mémoire (création d'une nouelle array)
 def shortest_element(data)
-	#& Opérateur de navigation sécurisée, il permet d’appeler des méthodes sur des objets sans s’inquiéter du fait que cet objet peut être nil (éviter une erreur undefined method for nil:NilClass), similaire à la méthode try dans Rails
+	# & Opérateur de navigation sécurisée, il permet d’appeler des méthodes sur des objets sans s’inquiéter du fait que cet objet peut être nil (éviter une erreur undefined method for nil:NilClass), similaire à la méthode try dans Rails
 	shortest = data.sort_by(&:length)[0] 
 	puts "the shortest handle is #{shortest}"
 	return shortest
@@ -30,7 +30,7 @@ puts "le nombre d'handles ayant 5 caractères est #{count_5}"
 return count_5
 end
 
-	# 4. Combien de handles commencent-ils avec une majuscule ?
+# 4. Combien de handles commencent-ils avec une majuscule ?
 def number_of_capitalized_handles(data)
 	count_upper = 0
 	data.each do |i|
@@ -57,20 +57,41 @@ def sort_by_length(data)
 	return sorted_by_length
 end
 
-# 7. position de l'array pour l'handle @epenser ?
+# 7. Déterminer la osition de l'array pour l'handle @epenser
 def spot_specific_handle(data)
 data.each do |i|
 	if i.to_s == "@epenser"
-		puts data.index(i)
+		puts "le handle '@epenser' est en postion #{data.index(i)}"
 	end
 end
 end
 
 # 8. Répartition des handles par taille (ordre croissant)
-# Array d'arrays [][]?
+# Array d'arrays [][] ?
 def number_of_handles_per_given_length(data)
-	#puts "on récupère bien #{sort_by_length(data)}"
+	# Créer un hash vide
+	#storing_hash = (1..sorted_data.length).each_with_object({}) { |i, h| h[i] = 0 }
+
+	# Créer un tableau de décompte vide
+	count_array=[]
+	(1..40).each do |i|
+		count_array[i] = 0
+	end
 	
+	# En itérant dans les données brutes, pour chaque entrée, prendre la longueur du pseudo et ajouter 1 unité au compteur de longeur de pseudo correspondant
+  data.each do |i|
+   longueur_du_pseudo =  i.length
+   count_array[longueur_du_pseudo-1] += 1
+  end
+  
+	# En itérant dans le compteur de longeur de pseudo
+  for i in (1..40)
+		# Si il existe au moins un pseudo contenant i caractères
+    if count_array[i] != 0  
+		  #donner le nombres de pseudos en question
+			puts "il a #{count_array[i]} handle(s) de #{i} caractères"
+    end
+  end
 end
 
 def perform(data)
@@ -79,10 +100,10 @@ def perform(data)
 	number_of_5_characters(data) #3
 	number_of_capitalized_handles(data) #4 
 	ordre_alpha(data) #5
-	sort_share = sort_by_length(data) #6
+	sort_by_length(data) #6
 	sort_by_length(data) 
 	spot_specific_handle(data) #7
-	number_of_handles_per_given_length(sort_share) #8
+	number_of_handles_per_given_length(data) #8
 end 
 
 perform(raw_data)
@@ -92,17 +113,17 @@ tips Cédric
 list = list.sort_by(&:length)
 sort_by{|word| word.length}
 tableau.sort_by{|word| word.length}
+
+tips mathias
+Commenter / décommenter: cmd-shift-slash
+
+Two right ways to right the same thing
+
+1.
+raw_data.each do |i| 
+	puts i.length
+end
+
+2
+raw_data.each {|i| puts i.length }
 =end
-
-#tips mathias
-#Commenter / décommenter: cmd-shift-slash
-
-# Two ways to right the same thing
-
-# 1
-#raw_data.each do |i| 
-#	puts i.length
-#end
-
-# 2
-#raw_data.each {|i| puts i.length }
